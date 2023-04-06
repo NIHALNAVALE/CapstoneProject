e85ddb5ea0120c8c6bfb23b7d60fbfe62e97d085
nihal <nihal.navale22@gmail.com>
2023-04-05 20:39:11 -0400

    turtlebot_ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/nihal/turtlebot_ws/src
+# Build directory: /home/nihal/turtlebot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense2_description")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("realsense_gazebo_plugin")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
